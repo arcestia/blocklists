9ed4f09af0a7f2590cfe0149f0e8553eae047085
Laurensius Jeffrey <jeff@skiddle.id>
2025-06-25 22:22:02 +0700

    chore: update

--- push.sh
+++ push.sh
@@ -6,10 +6,18 @@ SCRIPT_DIR="$(CDPATH='' cd -- "$(dirname -- "${0:?}")" && pwd -P)"
 
 main() {
 	updatedSources="$(git status --porcelain=v1 -- "${SCRIPT_DIR:?}/data/" | awk -F'/' '{printf("* %s\n",$2)}' | sort | uniq)"
-	if [ -n "${updatedSources?}" ]; then
-		commitMsg="$(printf '%s\n%s' 'Updated sources:' "${updatedSources:?}")"
+	updatedReleased="$(git status --porcelain=v1 -- "${SCRIPT_DIR:?}/released/" | awk -F'/' '{printf("* %s\n",$2)}' | sort | uniq)"
+	commitMsg=""
+	if [ -n "${updatedSources}" ]; then
+		commitMsg="$(printf '%s\n%s' 'Updated sources:' "${updatedSources}")"
 		git add -- "${SCRIPT_DIR:?}/data/"
-		git commit -m "${commitMsg:?}"
+	fi
+	if [ -n "${updatedReleased}" ]; then
+		commitMsg="$commitMsg\n$(printf '%s\n%s' 'Updated released:' "${updatedReleased}")"
+		git add -- "${SCRIPT_DIR:?}/released/"
+	fi
+	if [ -n "$commitMsg" ]; then
+		git commit -m "$commitMsg"
 		git push origin HEAD
 	fi
 }
